66db92adc1603cb170ccdb94a412c268780d1f1f
phdotexe <pharrybiobele@gmail.com>
2025-02-13 00:09:12 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/playful-puzzles/sw.js', { scope: '/playful-puzzles/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
